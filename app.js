3a0dd76e2924ceca3473c188a4f96facc836f950
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-03-05 00:36:19 +0500

    Phase 1 Completed

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Listing = require("./models/Listing.js")
 const ejsMate = require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js")
+const {listingSchemas} = require("./schema.js")
 
 // EXPRESS REQUIREMENTS
 
@@ -29,6 +32,15 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/roomio');
 }
 
+// Joi Validation Function
+    validateListing = async(req,res,next)=>{
+            try {
+        const value = await listingSchemas.validateAsync(req.body);
+    }
+    catch (err) {
+        throw new ExpressError(400, err.message)
+     }
+    }
 
 // Express App
 
@@ -37,62 +49,78 @@ app.get("/",(req,res)=>{
 })
 
 // Index Route
-app.get("/listing",async (req,res)=>{
+app.get("/listing",wrapAsync(async (req,res)=>{
     let datas = await Listing.find()
     res.render("listings/listing.ejs",{datas})
-})
+}))
 
 
 // New Hotel Route - Specific Route
-app.get("/listing/new",async (req,res)=>{
+app.get("/listing/new",wrapAsync(async (req,res)=>{
     res.render("listings/new.ejs")
-})
+}))
 
 // Note Specific Route ka hameesha dynamic sy upar rkho
 
 // Adding DATA - POST Route
+// Error Handled as Async Function so adding Next in the catch 
 
-app.post("/listing",async (req,res)=>{
-    let data = req.body;
+
+app.post("/listing",validateListing,wrapAsync(async (req,res)=>{
+    console.log(rescult)
+    let data = req.body.listing;
     await Listing.insertOne(data)
     res.redirect("/listing")
-})
+}))
 
 // Editing Using PUT Request
-app.put("/listing/:id",async(req,res)=>{
+app.put("/listing/:id",validateListing,wrapAsync(async(req,res)=>{
     let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,req.body)
+    await Listing.findByIdAndUpdate(id,req.body.listing)
     res.redirect("/listing")
-})
+}))
 
 // Deleting Using DELETE Request
-app.delete("/listing/:id",async(req,res)=>{
+app.delete("/listing/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndDelete(id)
     res.redirect("/listing")
-})
+}))
 
 // Editing with Dynamic Route
 
-app.get("/listing/:id/edit",async (req,res)=>{
+app.get("/listing/:id/edit",wrapAsync(async (req,res)=>{
     let {id} = req.params;
     let data = await Listing.findById(id)
     res.render("listings/edit.ejs",{data})
-})
+}))
 
 // Show Route - Dynamic Route
-app.get("/listing/:id",async (req,res)=>{
+app.get("/listing/:id",wrapAsync(async (req,res)=>{
     let {id} = req.params;
     let data = await Listing.findById(id)
     res.render("listings/show.ejs",{data})
-})
+}))
+// 404 Page Error Throw 
+// If the user is sending the request on any page which doesnot exist so we will use it
+// app.all with * will check all above routes first and then will give the response if any of the above response doesnot match 
+
+
+// We learnt a new thing Express k purany version meen woh "*" ko use kr leta tha ab nhi ab is k lye you have to use /.*/ is sy us ko yeh pta lg jay ga that it is originally Regular Expression he 
+
+// 404 Page Error Throw 
+// Use a regex literal (no quotes) to bypass the strict string parser
+app.all(/.*/, (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
 
 // Adding an Error Handling Middleware
 
 app.use((err,req,res,next)=>{
-    let{status=500,message} = err;
-    res.status(status).send(message)
-    next()
+    let {status=500,message} = err;
+    // res.status(status).send(message)
+    res.status(status)
+    res.render("error.ejs",{message})
 })
 
 // Listening
