9272dd358c17a4b3662275273f5e5848ac96994a
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-03-28 01:46:26 +0500

    Phase 2

--- classroom/server.js
+++ classroom/server.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const session = require('express-session')
+app.use(session({
+    secret: "mysupersecretstring",
+    resave:false,
+    saveUninitialized:true
+}))
+
+
+app.get("/get",(req,res)=>{
+    res.send("Hi")
+})
+
+app.get("/reqcount",(req,res)=>{
+    // req.session.count = 1; //Variable store kya he 
+    if(req.session.count){
+        req.session.count++;
+    }
+    else{
+        req.session.count = 1;
+    }
+
+    res.send(req.session.count)
+})
 
 
 // Listening
