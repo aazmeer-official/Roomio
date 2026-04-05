34352676848c844e08ff84836deaacce3dbf205c
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-04-06 04:48:32 +0500

    features: Maps Added

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/Listing")
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 // Index Route 
 module.exports.index = async (req,res)=>{
     let datas = await Listing.find()
@@ -10,12 +13,19 @@ module.exports.renderNewForm = async (req,res)=>{
 }
 // Adding Data
 module.exports.addData = async (req, res) => {
+    let response = await geocodingClient.forwardGeocode({
+    query: req.body.listing.location,
+    limit: 1
+    })
+    .send()
     let url = req.file.path;
     let filename = req.file.filename;
     let newListing = new Listing(req.body.listing); // Create instance
     newListing.owner = req.user._id;
     newListing.image = {url,filename}
-    await newListing.save(); // Save to DB
+    newListing.geometry = response.body.features[0].geometry
+    let savedListing = await newListing.save(); // Save to DB
+    console.log(savedListing)
     req.flash("success", "New Listing Created");
     res.redirect("/listing");
 }
