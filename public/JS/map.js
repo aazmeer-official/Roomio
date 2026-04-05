b6269d8427a1917ff044765f0b592cd14f1219b3
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-04-06 04:58:15 +0500

    VS CODE ERROR FIXED

--- public/JS/map.js
+++ public/JS/map.js
@@ -1,15 +1,15 @@
+const mapData = document.getElementById('map-data');
+const coordinates = JSON.parse(mapData.dataset.coordinates);
+const listingLocation = mapData.dataset.location;
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: "map", // container ID
-  // style:"mapbox://styles/mapbox/dark-v11",
-  center: data.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9, // starting zoom
+  container: "map",
+  center: coordinates,
+  zoom: 9,
 });
 
-console.log(data.geometry.coordinates)
-// Create a default Marker and add it to the map.
 const marker = new mapboxgl.Marker({ color: 'red', rotation: 45 })
-  .setLngLat(data.geometry.coordinates) //Lising.geometry.coordinates
-  .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h3>${data.location}</h3><p>Exact Location Provided after the booking </p>`))
-  
+  .setLngLat(coordinates)
+  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${listingLocation}</h3><p>Exact Location Provided after the booking</p>`))
   .addTo(map);
